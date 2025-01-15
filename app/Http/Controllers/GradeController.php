e58f7a61ff46f1ff74acb11180b9e2da19a4cdf0
Renate <97016086@st.deltion.nl>
2025-01-15 09:21:21 +0100

    added validation to check if the teacher is teaching the subject.

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use	App\Models\Grade;
 use App\Models\Klas;
-
+use App\Models\Subject;
+use App\Models\User;
 
 class GradeController extends Controller
 {
@@ -27,7 +28,7 @@ class GradeController extends Controller
 			$gradesQuery->where('block',	$block);
 		}
 
-		//Haal	de	cijfers	met hun relaties op
+		//	Haal	de	cijfers	met hun relaties op
 		$grades	=	$gradesQuery->with(['student',	'subject',	'teacher'])->get();
 
 		return	view('grades.index',	compact('grades'));
@@ -37,14 +38,53 @@ class GradeController extends Controller
 	public	function	store(Request	$request)
 	{
 
-		//Validatie van invoer
+		//	Validatie van invoer
 		$request->validate([
 			'student_id'	=>	'required|exists:students,id',
 			'subject_id'	=>	'required|exists:subjects,id',
 			'score'	=>	'required|numeric|min:1|max:10',
 			'block'	=>	'required|string|max:10'
 		]);
-	}
 
-	public	function	update(Request	$request, string	$id) {}
+		//	Validatie of de docent het vak geeft
+		$subject	=	Subject::findOrFail($request->subject_id);
+		if ($subject->teacher_id	!==	auth()->user()->id) {
+			abort(403, 'Je mag geen cijfers invoeren voor dit vak !');
+		}
+
+		//	Maak een niew cijfer aan 
+		Grade::create([
+			'student_id'	=>	$request->student_id,
+			'subject_id'	=>	$request->subject_id,
+			'teacher_id'	=>	auth()->user()->id,
+			'score'	=>	$request->score,
+			'block'	=>	$request->block
+		]);
+
+		return	redirect()->back()->with('succes', 'Cijfer succesvol toegevoegd.');
+	}
+	//	Werk een bestaand cijfer bij. 
+	public	function	update(Request	$request, string	$id)
+	{
+
+		//	Validatie van invoer
+		$request->validate([
+			'score'	=>	'required|numeric|min:1|max:10',
+		]);
+
+		//	Haal het bestaande cijfer op
+		$grade	=	Grade::findOrFail($id);
+
+		//	Controleer of de leraar dit cijfer mag wijzigen.
+		if ($grade->teacher->id	!==	auth()->user()->teacher->id) {
+			abort(403, "Je mag dit cijfer niet wijzigen");
+		}
+
+		//	Update het cijfer
+		$grade->update([
+			'score'	=>	$request->score,
+		]);
+
+		return	redirect()->back()->with('succes', 'Cijfer succesvol bijgewerkt.');
+	}
 }
